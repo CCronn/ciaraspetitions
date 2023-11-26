c17cc5cf7803857937e1ce5a7a4fcda78db5afcd
Ciara <ciara.cronin.00@gmail.com>
2023-11-26 18:17:02 +0000

    Add JS for search page to listen for click, call API and display results.

---
+++ src/main/resources/static/search.js
@@ -0,0 +1,22 @@
+
+// when html is parsed,
+document.addEventListener("DOMContentLoaded", function () {
+    const searchForm = document.getElementById("searchForm");
+
+    // when the search button is clicked
+    searchForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        //extract keyword
+        const searchKeyword = document.getElementById("searchKeyword").value;
+
+        // Query API endpoint for searching title & description
+        fetch(`/petitions/search?keyword=${encodeURIComponent(searchKeyword)}`)
+            .then(response => response.json())
+            .then(data => {
+                // call function to display
+                displayPetitions(data);
+            })
+            .catch(error => console.error("Error fetching search results:", error));
+    });
+});
