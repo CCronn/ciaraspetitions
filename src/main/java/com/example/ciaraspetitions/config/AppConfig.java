77b7f6a10c64abcec4ba5437a2c7f635b6472bb0
Ciara <ciara.cronin.00@gmail.com>
2023-11-26 16:52:47 +0000

    Add bean to AppConfig to enable dummy data.

--- src/main/java/com/example/ciaraspetitions/config/AppConfig.java
+++ src/main/java/com/example/ciaraspetitions/config/AppConfig.java
@@ -1,8 +1,16 @@
 package com.example.ciaraspetitions.config;
 
+import com.example.ciaraspetitions.repository.PetitionRepository;
+import com.example.ciaraspetitions.service.DummyDataSeeder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public DummyDataSeeder dummyDataSeeder(PetitionRepository petitionRepository) {
+        return new DummyDataSeeder(petitionRepository);
+    }
 
 }
