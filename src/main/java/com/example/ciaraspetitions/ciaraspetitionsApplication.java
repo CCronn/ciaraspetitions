2bc4ef4c62318ed8eca46bf333e909e212140244
Ciara <ciara.cronin.00@gmail.com>
2023-11-26 15:09:47 +0000

    Initial commit

---
+++ src/main/java/com/example/ciaraspetitions/ciaraspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ciaraspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ciaraspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ciaraspetitionsApplication.class, args);
+	}
+
+}
